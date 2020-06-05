a8fa0571f625e61f910ee56157e20210970a6176
Nicoleta <nicoleta.platniuc@gmail.com>
2020-06-05 18:15:36 -0500

    moving the routes configs to separate file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,20 +13,9 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const routes : Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent, children: [
-    {path: ':id/:name', component: UserComponent}
-  ]},
-  {path: 'servers', component: ServersComponent, children: [
-    {path: ':id', component: ServerComponent},
-    {path: ':id/edit', component: EditServerComponent}
-  ]},
-  {path: 'not-found', component: PageNotFoundComponent},
-  // path '**' always should be the last
-  {path: '**', redirectTo: '/not-found'}
-];
+
 
 @NgModule({
   declarations: [
@@ -42,7 +31,7 @@ const routes : Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
