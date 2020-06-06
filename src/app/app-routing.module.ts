1298a0c7e6ab064766c815429977625b19828fcb
Nicoleta <nicoleta.platniuc@gmail.com>
2020-06-06 13:54:37 -0500

    canDeactivate, passing static/dinamic data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { Resolver } from "dns";
+import { ServerResolver } from "./servers/server-resolver.service";
 
 const routes : Routes = [
     {path: '', component: HomeComponent},
@@ -19,16 +23,18 @@ const routes : Routes = [
     //canActivate: [AuthGuard],
     canActivateChild: [AuthGuard],
      component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}
+      {path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
+      {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]},
-    {path: 'not-found', component: PageNotFoundComponent},
+    //{path: 'not-found', component: PageNotFoundComponent},
+    {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page Not Found! '}  },
     // path '**' always should be the last
     {path: '**', redirectTo: '/not-found'}
   ];
 
 @NgModule({
     imports: [
+        // RouterModule.forRoot(routes, {useHash: true})
         RouterModule.forRoot(routes)
     ],
     exports: [
